78455a0cf2e97db646c881f1cf5c988c20be90b5
Charles <charleslanop@gmail.com>
2022-01-30 11:11:33 -0300

    handling errors

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -5,12 +5,12 @@ const errorMiddleware = (
   error: Error,
   request: Request,
   response: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   next: NextFunction
 ) => {
   const status = error.status || 500;
   const message = error.message || 'Whoops!!! something went wrong';
   response.status(status).json({ status, message });
-  next();
 };
 
 export default errorMiddleware;
